2e8a1422a507bf68cc19debd759c72b0891e3faf
J. L. B. Donin <88415069+luisdonin@users.noreply.github.com>
2023-07-12 11:29:17 -0300

    Added index bar

--- script.js
+++ script.js
@@ -85,3 +85,10 @@ backToTopBtn.addEventListener('click', () => {
     });
 });
 
+// Get the index div
+const indexDiv = document.querySelector('.index');
+
+// Add event listener to toggle the menu
+indexDiv.addEventListener('click', function () {
+  indexDiv.classList.toggle('collapsed');
+});
